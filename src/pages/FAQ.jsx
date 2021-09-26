f6059188774cb85fb8034c55b7205fbbc3b700a0
nndeto <86444679+nndeto@users.noreply.github.com>
2021-09-25 22:22:03 -0400

    styled faq page

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,8 +1,20 @@
+import '../css/FAQ.scss'
+
 function FAQ() {
     return(
-        <div>
-            <h3>Eventually, I'll hold FAQs</h3>
-        </div>
+        <main className = "f-main">
+            <h1 className = "f-title">FAQ</h1>
+            <div className = "f-content">
+                <h2 className = "f-headers">What is Roo?</h2>
+                <p className="f-answers">Arabica Acerbic Affogato Aftertaste Aged Americano And Aroma, wings caffeine viennese doppio press plunger whipped and, dark con spoon robusta sugar grinder. And seasonal chicory affogato ut sugar cappuccino siphon, robust strong java french Aged mocha as, roast breve at Arabica flavour Brewed. Et so doppio Acerbic grounds kopi-luwak sweet macchiato black flavour mocha iced, foam decaffeinated pumpkin roast coffee organic breve fair variety.</p>
+                <h2 className = "f-headers">Why choose Roo?</h2>
+                <p className="f-answers">Arabica Acerbic Affogato Aftertaste Aged Americano And Aroma, wings caffeine viennese doppio press plunger whipped and, dark con spoon robusta sugar grinder. And seasonal chicory affogato ut sugar cappuccino siphon, robust strong java french Aged mocha as, roast breve at Arabica flavour Brewed. Et so doppio Acerbic grounds kopi-luwak sweet macchiato black flavour mocha iced, foam decaffeinated pumpkin roast coffee organic breve fair variety.</p>
+                <h2 className = "f-headers">Where is Roo?</h2>
+                <p className="f-answers">Arabica Acerbic Affogato Aftertaste Aged Americano And Aroma, wings caffeine viennese doppio press plunger whipped and, dark con spoon robusta sugar grinder. And seasonal chicory affogato ut sugar cappuccino siphon, robust strong java french Aged mocha as, roast breve at Arabica flavour Brewed. Et so doppio Acerbic grounds kopi-luwak sweet macchiato black flavour mocha iced, foam decaffeinated pumpkin roast coffee organic breve fair variety.</p>
+                <h2 className = "f-headers">Isn't Our Name Fun?</h2>
+                <p className="f-answers">Arabica Acerbic Affogato Aftertaste Aged Americano And Aroma, wings caffeine viennese doppio press plunger whipped and, dark con spoon robusta sugar grinder. And seasonal chicory affogato ut sugar cappuccino siphon, robust strong java french Aged mocha as, roast breve at Arabica flavour Brewed. Et so doppio Acerbic grounds kopi-luwak sweet macchiato black flavour mocha iced, foam decaffeinated pumpkin roast coffee organic breve fair variety.</p>
+            </div>
+        </main>
     )
 }
 
